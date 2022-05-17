cac7c7965a725e71a136c97f5c77d780e7e4b81c
maddrake220 <jwchoi4118@gmail.com>
2022-05-18 00:41:46 +0900

    feat) project init

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>DECODE LAB</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Content from "../content/Content";
+import Header from "../header/Header";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Content />
+    </div>
+  );
+};
+
+export default Home;
