86f37d2cfd8c754bb864ccc613b9755c319693e0
maddrake220 <jwchoi4118@gmail.com>
2022-05-18 13:13:15 +0900

    feat: header

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,7 +1,23 @@
 import React from "react";
-
+import "./content.scss";
 const Content = () => {
-  return <main>Content</main>;
+  return (
+    <main>
+      <ul>
+        <li>
+          <a href="#" className="on">
+            ALL
+          </a>
+        </li>
+        <li>
+          <a href="#">ODD</a>
+        </li>
+        <li>
+          <a href="#">EVENT</a>
+        </li>
+      </ul>{" "}
+    </main>
+  );
 };
 
 export default Content;
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,30 @@
 import React from "react";
-
+import "./header.scss";
 const Header = () => {
   return (
     <header>
       <h1>DECODE LAB</h1>
+
+      <ul>
+        <li>
+          <a href="#">
+            <i className="fab fa-twitter-square"></i>
+            <span>Twitter</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i className="fab fa-facebook-f"></i>
+            <span>Facebook</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i className="fas fa-envelope"></i>
+            <span>E-mail</span>
+          </a>
+        </li>
+      </ul>
     </header>
   );
 };
